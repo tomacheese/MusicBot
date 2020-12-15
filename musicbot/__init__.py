de9de9bc6dc4f2791a795008fdb77eef63cd2c73
rexjohannes <53578137+rexjohannes@users.noreply.github.com>
2020-12-15 19:18:57 +0100

    Update __init__.py

--- musicbot/__init__.py
+++ musicbot/__init__.py
@@ -102,12 +102,12 @@ def _add_logger_level(levelname, level, *, func_name = None):
 
 
 #_add_logger_level('EVERYTHING', 1)
-_add_logger_level('NOISY', 4, func_name='noise')
-_add_logger_level('FFMPEG', 5)
-_add_logger_level('VOICEDEBUG', 6)
+#_add_logger_level('NOISY', 4, func_name='noise')
+#_add_logger_level('FFMPEG', 5)
+#_add_logger_level('VOICEDEBUG', 6)
 
 log = logging.getLogger(__name__)
-log.setLevel(logging.EVERYTHING)
+#log.setLevel(logging.EVERYTHING)
 
 fhandler = logging.FileHandler(filename='logs/musicbot.log', encoding='utf-8', mode='a')
 fhandler.setFormatter(logging.Formatter(
